9fbfb1fa55ad625d0bf8c92e50b6d260f4b020f2
kuwalab <arata.shinichi@gmail.com>
2014-07-29 20:10:50 +0900

    最初のコミット

---
+++ l_chika/L_chika.ino
@@ -0,0 +1,27 @@
+const int LED = 13;
+
+void setup()
+{
+  pinMode(LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LED, HIGH);
+  delay(200);
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(200);
+  digitalWrite(LED, LOW);
+  delay(200);
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  delay(1000);
+}
+
